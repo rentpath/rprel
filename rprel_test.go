ab0fe2b441e5d834b2acc80b5ec8157de273538e
Teapane <tyler.stephen.long@gmail.com>
2016-05-26 13:16:37 -0600

    [TL][119184991] Adding tests

--- rprel_test.go
+++ rprel_test.go
@@ -1,76 +1,92 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	// "encoding/json"
+	// "fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
-	//"net/url"
-	"reflect"
+	// "reflect"
+	"strings"
 	"testing"
 )
 
 var (
-	// mux is the HTTP request multiplexer used with the test server.
-	mux *http.ServeMux
-
-	// client is the GitHub client being tested.
-	client *Release
-
-	// server is a test HTTP server used to provide mock API responses.
 	server *httptest.Server
+	reader io.Reader
+	input  string
 )
 
-func NewClient(httpClient *http.Client) *Release {
-	if httpClient == nil {
-		httpClient = http.DefaultClient
+func test(t *testing.T) {
+	userJson := `{"Name": "test", "commitish": "master"}`
+
+	reader := strings.NewReader(userJson) //Convert string to reader
+
+	request, err := generateRelease("POST", input, reader) //Create request with JSON body
+
+	res, err := http.DefaultClient.Do(request)
+
+	if err != nil {
+		t.Error(err) //Something is wrong while sending request
 	}
 
-	return nil
-}
-
-func setup() {
-	// test server
-	mux = http.NewServeMux()
-	server = httptest.NewServer(mux)
-
-	// github client configured to use test server
-	client = NewClient(nil)
-	//url, _ := url.Parse(server.URL)
-	//	client.BaseURL = url
-	//client.UploadURL = url
-}
-
-func testMethod(t *testing.T, r *http.Request, want string) {
-	if got := r.Method; got != want {
-		t.Errorf("Request method: %v, want %v", got, want)
+	if res.StatusCode != 201 {
+		t.Errorf("Success expected: %d", res.StatusCode) //Uh-oh this means our test failed
 	}
 }
 
-func TestRepositoriesService_CreateRelease(t *testing.T) {
-	setup()
-	defer server.Close()
+// func testrepositoriesservice_createrelease(t *testing.T) {
+//	setup()
+//	defer server.close()
 
-	input := &Release{Name: "v1.0"}
+//	input := &release{name: string("v1.0")}
 
-	mux.HandleFunc("/repos/o/r/releases", func(w http.ResponseWriter, r *http.Request) {
-		v := new(Release)
-		json.NewDecoder(r.Body).Decode(v)
+//	mux.handlefunc("/repos/o/r/releases", func(w http.responsewriter, r *http.request) {
+//		v := new(release)
+//		json.newdecoder(r.body).decode(v)
 
-		testMethod(t, r, "POST")
-		if !reflect.DeepEqual(v, input) {
-			t.Errorf("Request body = %+v, want %+v", v, input)
-		}
-		fmt.Fprint(w, `{"id":1}`)
-	})
+//		testmethod(t, r, "post")
+//		if !reflect.deepequal(v, input) {
+//			t.errorf("request body = %+v, want %+v", v, input)
+//		}
+//		fmt.fprint(w, `{"id":1}`)
+//	})
 
-	// release, _, err := client.generateRelease("o", "r", input)
-	// if err != nil {
-	//	t.Errorf("Repositories.CreateRelease returned error: %v", err)
-	// }
+//	release, _, err := githubpostrequest("o", "r", input)
+//	if err != nil {
+//		t.errorf("repositories.createrelease returned error: %v", err)
+//	}
 
-	// want := &RepositoryRelease{ID: Int(1)}
-	// if !reflect.DeepEqual(release, want) {
-	//	t.Errorf("Repositories.CreateRelease returned %+v, want %+v", release, want)
-	// }
-}
+//	want := &release{name: string("test")}
+//	if !reflect.deepequal(release, want) {
+//		t.errorf("repositories.createrelease returned %+v, want %+v", release, want)
+//	}
+// }
+
+// func testmethod(t *testing.t, r *http.request, want string) {
+//	if got := r.method; got != want {
+//		t.errorf("request method: %v, want %v", got, want)
+//	}
+// }
+
+// var (
+//	// mux is the HTTP request multiplexer used with the test server.
+//	mux *http.ServeMux
+
+//	// client is the GitHub client being tested.
+//	client = http.Client{}
+//	// server is a test HTTP server used to provide mock API responses.
+//	server *httptest.Server
+// )
+
+// func setup() {
+//	// test server
+//	mux = http.NewServeMux()
+//	server = httptest.NewServer(mux)
+
+//	// github client configured to use test server
+//	client = http.Client{}
+//	url, _ := url.Parse(server.URL)
+//	client.BaseURL = url
+//	client.UploadURL = url
+// }

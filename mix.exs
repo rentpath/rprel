56b9bf65240281b2da6d21ca4240f8800ff7096f
Eric Toulson <ebtoulson@gmail.com>
2017-10-19 15:46:18 -0400

    Bumping version to v2.2.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Rprel.Mixfile do
 
   def project do
     [app: :rprel,
-     version: "2.2.1",
+     version: "2.2.2",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,

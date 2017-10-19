d02d45bf7b84c984fac6096a4cab3cb1fa11d9f9
Eric Toulson <ebtoulson@gmail.com>
2017-10-19 12:13:50 -0400

    updating elixir, locking down problematic deps

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rprel.Mixfile do
   def project do
     [app: :rprel,
      version: "2.2.1",
-     elixir: "~> 1.4.0",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      escript: [main_module: Rprel.CLI],
@@ -32,15 +32,17 @@ defmodule Rprel.Mixfile do
   end
 
   defp deps do
-    [{:bypass, "~> 0.7.0", only: :test},
+    [{:bypass, "~> 0.8.0", only: :test},
      {:credo, "~> 0.4", only: [:dev, :test]},
      {:ex_doc, ">= 0.0.0", only: :dev},
      {:httpoison, "~> 0.8"},
      {:mix_test_watch, "~> 0.2", only: :dev},
-     {:poison, "~>2.0"},
+     {:poison, "~> 3.1"},
      {:porcelain, "~> 2.0.0"},
      {:timex, "~> 3.1.7"},
-     {:tzdata, "~> 0.1.8"},
-     {:uri_template, "~>1.0"}]
+     # newer tzdata doesn't work from within an escript because it uses ETS
+     {:tzdata, "~> 0.1.8", override: true},
+     # examples don't even work on newer versions for uri_template
+     {:uri_template, "~> 1.0.0"}]
   end
 end
